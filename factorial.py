07b55f1fcf7e2c4393e80089dcbc10cd93d3245c
unknown <timone300003@gmail.com>
2023-09-07 01:06:53 +0300

    сделали решение факториала цикличным

--- factorial.py
+++ factorial.py
@@ -1,10 +1,10 @@
 
 
 def fact(n):
-    if n == 0:
-        return 1
-    else:
-        return n * fact(n - 1)
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+    return factorial
 
 
 n = int(input())
