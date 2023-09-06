5b57216d82886e19792a4474149fce518427bf1c
unknown <timone300003@gmail.com>
2023-09-07 00:42:40 +0300

    сделали решениефакториала на С#

---
+++ Factorial/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+int Fact(int num){
+    if(num == 0){
+        return 1;
+    }
+    return num * Fact(num - 1);
+}
+
+
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(Fact(n));
